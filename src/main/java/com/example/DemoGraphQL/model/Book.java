d544908e86b91c13e5feee09c9342ac5ccc6097e
Rohan Dongre <dongrerohan421@gmail.com>
2019-11-10 15:36:47 -0500

    create Author and Book entities

---
+++ src/main/java/com/example/DemoGraphQL/model/Book.java
@@ -0,0 +1,24 @@
+package com.example.DemoGraphQL.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    private int pageCount;
+}
