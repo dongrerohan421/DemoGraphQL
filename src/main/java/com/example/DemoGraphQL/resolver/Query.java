4cf661de9e145e1eb0cfeb5f063993865a445248
Rohan Dongre <dongrerohan421@gmail.com>
2019-11-10 16:22:37 -0500

    GraphQLQueryResolver to define the operations of the root Query type

---
+++ src/main/java/com/example/DemoGraphQL/resolver/Query.java
@@ -0,0 +1,35 @@
+package com.example.DemoGraphQL.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.example.DemoGraphQL.model.Author;
+import com.example.DemoGraphQL.model.Book;
+import com.example.DemoGraphQL.repository.AuthorRepository;
+import com.example.DemoGraphQL.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Query implements GraphQLQueryResolver {
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    public Query(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    public Iterable<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Iterable<Author> findAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public long countBooks() {
+        return bookRepository.count();
+    }
+
+    public long countAuthors() {
+        return authorRepository.count();
+    }
+}
