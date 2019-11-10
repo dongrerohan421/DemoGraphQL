473217617fe13e31432ad1392f720de01af9d143
Rohan Dongre <dongrerohan421@gmail.com>
2019-11-10 15:42:35 -0500

    add Author field in Book entity

--- src/main/java/com/example/DemoGraphQL/model/Book.java
+++ src/main/java/com/example/DemoGraphQL/model/Book.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,4 +18,7 @@ public class Book {
     private String title;
     private String isbn;
     private int pageCount;
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false, updatable = false)
+    private Author author;
 }
