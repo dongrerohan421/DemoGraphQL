7c24ad0636e12e3387b92b33be20f9def33f6d7d
Rohan Dongre <dongrerohan421@gmail.com>
2019-11-10 16:20:22 -0500

    add constructor, getter, setter

--- src/main/java/com/example/DemoGraphQL/model/Author.java
+++ src/main/java/com/example/DemoGraphQL/model/Author.java
@@ -1,17 +1,11 @@
 package com.example.DemoGraphQL.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 public class Author {
     @Id
@@ -19,4 +13,62 @@ public class Author {
     private Long id;
     private String firstName;
     private String lastName;
+
+    public Author() {
+    }
+
+    public Author(Long id) {
+        this.id = id;
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
